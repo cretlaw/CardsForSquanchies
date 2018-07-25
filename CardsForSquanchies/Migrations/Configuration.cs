5bdf5b5d6389352dce3f593416c1d3d4e88dd606
winterMute84 <wcarbajal84@gmail.com>
2018-07-24 20:36:53 -0700

    added IntialModel to migrations

---
+++ CardsForSquanchies/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace CardsForSquanchies.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<CardsForSquanchies.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(CardsForSquanchies.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
