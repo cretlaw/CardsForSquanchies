2745e8bc18a6121f2010e4e1bb76bb0ef17631b8
winterMute84 <wcarbajal84@gmail.com>
2018-07-24 20:20:20 -0700

    Add project files.

---
+++ CardsForSquanchies/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CardsForSquanchies.Startup))]
+namespace CardsForSquanchies
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
