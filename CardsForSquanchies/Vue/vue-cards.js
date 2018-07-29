ade2d4f4fe59538c1a6846213046ca882850f8d0
winterMute84 <wcarbajal84@gmail.com>
2018-07-28 18:14:11 -0700

    Installed Vue and finished prototyping cards index page

---
+++ CardsForSquanchies/Vue/vue-cards.js
@@ -0,0 +1,26 @@
+﻿new Vue({
+    el: "#CardsApp",
+    data: {
+        cards: [],
+        rowDisplay:true
+    },
+    created: function () {
+
+        let vm = this;
+
+        axios.get("http://localhost:52007/api/cards")
+            .then(response => {
+                this.cards = response.data.slice()
+            })
+    },
+    methods: {
+        toggleRow: function (index) {
+            if (index % 3 === 0) {
+                this.rowDisplay = true
+            } else {
+                this.rowDisplay = false
+            }
+            
+        }
+    }
+})
